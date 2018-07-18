4e3d8acd1d227edff1052daf80e1b9b693bfa098
duhang <840261937@qq.com>
2018-07-18 18:02:52 +0800

    关于头图

--- pages/about.vue
+++ pages/about.vue
@@ -1,20 +1,29 @@
 <template>
     <div class="about">
         <div class="container">
-            <div class="content ql-editor" v-html="about"></div>
+            <div class="banner">
+                <img v-if="aboutBanner.length" :src="aboutBanner | qiniuDomain" alt="">
+            </div>
+            <div class="content">
+                <div class="ql-editor" v-html="about"></div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import {blog_name} from '~/config/blog_config'
+    import {domain} from '~/config/qiniu_config'
 
     export default {
         name: "about",
         asyncData({app}) {
             return app.$axios.$get('/api/individuation/getIndividuation').then((res) => {
                 if (res.code === 200) {
-                    return {about: res.data.about};
+                    return {
+                        about: res.data.about,
+                        aboutBanner: res.data.about_banner
+                    };
                 }
             });
         },
@@ -26,6 +35,11 @@
                     {hid: 'keyword', name: 'keyword', content: `${blog_name}, 关于, 博客介绍`}
                 ]
             }
+        },
+        filters: {
+            qiniuDomain(key) {
+                return `${domain}/${key}`;
+            }
         }
     }
 </script>
@@ -35,15 +49,26 @@
         padding: 6px 0;
         .container {
             min-height: calc(100vh - 122px);
-            padding: 0 10px 10px;
-            border-radius: 4px;
-            background-color: rgba(255, 255, 255, 0.9);
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
-            .ql-editor {
-                padding: 0;
-                font-size: 15px;
+            .banner {
+                margin-bottom: 6px;
+                border-radius: 4px;
+                overflow: hidden;
                 img {
-                    max-width: 100%;
+                    display: block;
+                    width: 100%;
+                }
+            }
+            .content {
+                padding: 10px;
+                border-radius: 4px;
+                background-color: rgba(255, 255, 255, 0.9);
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
+                .ql-editor {
+                    padding: 0;
+                    font-size: 15px;
+                    img {
+                        max-width: 100%;
+                    }
                 }
             }
         }
